c893c30cc1693b30ecdafd6f3a435b67e8c71f1e
nlucchi2307 <noemi.lucchi@bse.eu>
2025-10-14 19:14:23 +0400

    minor change

--- forwarder_eng.py
+++ forwarder_eng.py
@@ -4,9 +4,6 @@ import re
 import telethon
 from telethon import TelegramClient, events
 
-from dotenv import load_dotenv
-load_dotenv()
-
 # === CONFIG ===
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
